6cc1b362ecc76dcc59422fb67cb97bf117b9574c
Julia <vorobchenko.julia@gmail.com>
2021-11-21 22:13:21 +0200

    lab2 almost finished

---
+++ lab2.hs
@@ -0,0 +1,28 @@
+{-
+Завдання
+Визначте вказанi функцiї в кожному з завдань: а) без застосування, б) з
+застосуванням вбудованих функцiй.
+1.2 Знайти передостаннiй елемент списку.
+2.2 Циклiчний лiвий зсув списку на n позицiй.
+-}
+
+import Data.List
+import System.IO
+
+randTuple = [1, 2, 3, 4, 5]
+
+beforeLast1 :: [Integer] -> Integer
+beforeLast1 (some_tuple) = some_tuple !! (length(some_tuple)-2)
+
+beforeLast2 :: [Integer] -> Integer
+beforeLast2 [] = 0
+beforeLast2 (x:xs) = if length(xs) == 1 then x
+    else beforeLast2 xs
+
+cycleShift1::([Integer],Int) -> [Integer]  
+cycleShift1 (xs, n::Int) = if n == 0 then xs
+    else cycleShift1(tail(xs) ++ [head(xs)], (n-1))
+
+-- cycleShift2::([Integer],Int) -> [Integer]  
+-- cycleShift2 (xs, n::Int) = if n == 0 then xs
+--     else cycleShift2(tail(xs) ++ xs[0], (n-1))
